e29587500a8521a510d01d7413ed5810254b1a1b
sapkotau <usapkota@activehealth.net>
2017-01-31 17:33:59 -0500

    R with spark

--- R/RSpark.R
+++ R/RSpark.R
@@ -1,1 +1,12 @@
 # R with Spark
+
+#install.packages("sparklyr")
+
+library(sparklyr)
+spark_install(version = "1.6.2")
+
+# Connection to Spark
+sc <- spark_connect(master = "local")
+
+library(dplyr)
+iris_tbl <- copy_to(sc, iris)
